ac29d8c2fc1a7f35bffaf9dd5f67ca443ac91817
neurastra <157818039+neurastra@users.noreply.github.com>
2024-05-13 09:23:20 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,75 @@
-// место для вашего кода
+#pragma once
+
+#include <string>
+#include <deque>
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <unordered_set>
+#include <vector>
+#include <string_view>
+#include "geo.h"
+
+using Geo::Coordinates;
+
+namespace Data
+{
+    class TransportCatalogue
+    {
+        struct Bus;
+        struct Stop;
+        struct Bus_Hash;
+
+    public:
+        void add_bus(std::string_view name, std::vector<std::string_view> stops);
+        void add_stop(std::string_view name, Coordinates &&coordinates);
+        const Bus *get_bus(std::string_view bus) const;
+        const Stop *get_stop(std::string_view stop) const;
+        const std::unordered_set<Bus, Bus_Hash> &get_buses() const;
+        size_t get_stop_count(std::string_view bus) const;
+        size_t get_unique_stop_count(std::string_view bus) const;
+        double get_route_length(std::string_view bus) const;
+
+    private:
+        struct Stop
+        {
+            std::string name;
+            Coordinates coordinates;
+
+            bool operator==(const Stop &stop) const
+            {
+                return name == stop.name && coordinates == stop.coordinates;
+            }
+        };
+
+        struct Bus
+        {
+            std::string name;
+            std::deque<const Stop *> stops;
+
+            bool operator==(const Bus &bus) const
+            {
+                return name == bus.name;
+            }
+        };
+
+        struct Bus_Hash
+        {
+            size_t operator()(const Bus &bus) const
+            {
+                return std::hash<std::string>{}(bus.name);
+            }
+        };
+
+        struct Stop_Hash
+        {
+            size_t operator()(const Stop &stop) const
+            {
+                return static_cast<size_t>(static_cast<int>((stop.coordinates.lat * stop.coordinates.lng * 100000.0)));
+            }
+        };
+
+        std::unordered_set<Bus, Bus_Hash> buses_;
+        std::unordered_set<Stop, Stop_Hash> stops_;
+    };
+}
