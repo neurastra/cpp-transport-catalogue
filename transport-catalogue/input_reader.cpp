f5f894346496b5b4da3fba7b79b57df1c973849b
neurastra <157818039+neurastra@users.noreply.github.com>
2024-05-13 09:20:08 +0300

    Update input_reader.cpp

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -1,39 +1,113 @@
-#pragma once
-#include <string>
-#include <string_view>
-#include <vector>
+#include "input_reader.h"
+
 #include <algorithm>
-#include <execution>
-#include "geo.h"
-#include "transport_catalogue.h"
+#include <cassert>
+#include <iterator>
 
-namespace Input
+
+using Geo::Coordinates;
+using namespace Input;
+
+Coordinates ParseCoordinates(std::string_view str)
 {
-    struct CommandDescription
+    static const double nan = std::nan("");
+    auto not_space = str.find_first_not_of(' ');
+    auto comma = str.find(',');
+    if (comma == str.npos)
     {
-        explicit operator bool() const
-        {
-            return !command.empty();
-        }
+        return {nan, nan};
+    }
 
-        bool operator!() const
-        {
-            return !operator bool();
-        }
+    auto not_space2 = str.find_first_not_of(' ', comma + 1);
+    double lat = std::stod(std::string(str.substr(not_space, comma - not_space)));
+    double lng = std::stod(std::string(str.substr(not_space2)));
+    return {lat, lng};
+}
 
-        std::string command;     
-        std::string id;          
-        std::string description; 
-    };
-
-    class InputReader
+std::string_view Trim(std::string_view string)
+{
+    const auto start = string.find_first_not_of(' ');
+    if (start == string.npos)
     {
-    public:
-        void ParseLine(std::string_view line);
+        return {};
+    }
+    return string.substr(start, string.find_last_not_of(' ') + 1 - start);
+}
 
-        void ApplyCommands(Data::TransportCatalogue &catalogue) const;
+std::vector<std::string_view> Split(std::string_view string, char delim)
+{
+    std::vector<std::string_view> result;
 
-    private:
-        std::vector<CommandDescription> commands_;
-    };
+    size_t pos = 0;
+    while ((pos = string.find_first_not_of(' ', pos)) < string.length())
+    {
+        auto delim_pos = string.find(delim, pos);
+        if (delim_pos == string.npos)
+        {
+            delim_pos = string.size();
+        }
+        if (auto substr = Trim(string.substr(pos, delim_pos - pos)); !substr.empty())
+        {
+            result.push_back(substr);
+        }
+        pos = delim_pos + 1;
+    }
+
+    return result;
+}
+
+std::vector<std::string_view> ParseRoute(std::string_view route)
+{
+    if (route.find('>') != route.npos)
+    {
+        return Split(route, '>');
+    }
+
+    auto stops = Split(route, '-');
+    std::vector<std::string_view> results(stops.begin(), stops.end());
+    results.insert(results.end(), std::next(stops.rbegin()), stops.rend());
+    return results;
+}
+
+CommandDescription ParseCommandDescription(std::string_view line)
+{
+    auto colon_pos = line.find(':');
+    if (colon_pos == line.npos)
+    {
+        return {};
+    }
+
+    auto space_pos = line.find(' ');
+    if (space_pos >= colon_pos)
+    {
+        return {};
+    }
+
+    auto not_space = line.find_first_not_of(' ', space_pos);
+    if (not_space >= colon_pos)
+    {
+        return {};
+    }
+
+    return {std::string(line.substr(0, space_pos)),
+            std::string(line.substr(not_space, colon_pos - not_space)),
+            std::string(line.substr(colon_pos + 1))};
+}
+
+void InputReader::ParseLine(std::string_view line)
+{
+    auto command_description = ParseCommandDescription(line);
+    if (command_description)
+    {
+        commands_.push_back(std::move(command_description));
+    }
+}
+void InputReader::ApplyCommands([[maybe_unused]] Data::TransportCatalogue &catalogue) const
+{
+    for (const auto &command : commands_)
+        if (command.command == "Stop")
+            catalogue.add_stop(std::move(command.id), std::move(ParseCoordinates(command.description)));
+    for (const auto &command : commands_)
+        if (command.command == "Bus")
+            catalogue.add_bus(std::move(command.id), ParseRoute(command.description));
 }
