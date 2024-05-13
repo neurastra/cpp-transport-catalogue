5ae3e8d80d1d1fb78acf8b9529fc61de73d096cf
neurastra <157818039+neurastra@users.noreply.github.com>
2024-05-13 09:17:21 +0300

    Update input_reader.cpp

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -1,1 +1,39 @@
-// место для вашего кода
+#pragma once
+#include <string>
+#include <string_view>
+#include <vector>
+#include <algorithm>
+#include <execution>
+#include "geo.h"
+#include "transport_catalogue.h"
+
+namespace Input
+{
+    struct CommandDescription
+    {
+        explicit operator bool() const
+        {
+            return !command.empty();
+        }
+
+        bool operator!() const
+        {
+            return !operator bool();
+        }
+
+        std::string command;     
+        std::string id;          
+        std::string description; 
+    };
+
+    class InputReader
+    {
+    public:
+        void ParseLine(std::string_view line);
+
+        void ApplyCommands(Data::TransportCatalogue &catalogue) const;
+
+    private:
+        std::vector<CommandDescription> commands_;
+    };
+}
