291778006813216b646348fcd667156276f28dcc
neurastra <157818039+neurastra@users.noreply.github.com>
2024-06-02 14:32:40 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,84 +1,83 @@
 #include "stat_reader.h"
-#include <iomanip>
-#include <set>
-#include <math.h>
-
-using namespace std::string_view_literals;
-
-Request::Req Request::ParseRequest(const std::string_view request)
-{
-    Req result;
-    auto not_space = request.find_first_not_of(' ');
-    if (not_space == std::string_view::npos)
-        return {};
-    auto pos = request.find(' ', not_space);
-    result.request = request.substr(not_space, pos - not_space);
-    pos = request.find_first_not_of(' ', pos);
-    auto last_pos = request.find_last_not_of(' ');
-    result.what = request.substr(pos, last_pos - pos + 1);
-    return result;
+ 
+void query_bus(TransportCatalogue& catalogue, std::string_view str) {
+    auto entry = 4;
+    str = str.substr(entry);
+ 
+    Bus* bus = catalogue.get_bus(str);
+    if (bus != nullptr) {
+        std::cout << "Bus " << bus->name_ << ": "
+                  << bus->stops_.size() << " stops on route, "
+                  << (catalogue.get_uniq_stops(bus)).size() << " unique stops, "
+                  << catalogue.get_distance_to_bus(bus) << " route length, " 
+                  << std::setprecision(6) << double(catalogue.get_distance_to_bus(bus)
+                                                    /catalogue.get_length(bus))
+                  << " curvature" << std::endl;
+    } else {      
+        std::cout << "Bus " << str << ": not found" << std::endl;
+    }  
 }
-
-void Request::ParseAndPrintStat(const Data::TransportCatalogue &transport_catalogue, std::string_view request,
-                       std::ostream &output)
-{
-    auto parsed = Request::ParseRequest(request);
-    output << parsed.request << " "sv << parsed.what << ": "sv;
-    if (parsed.request == "Bus"sv)
-    {
-        if (transport_catalogue.get_bus(parsed.what) == nullptr)
-        {
-            output << "not found\n"sv;
+ 
+void query_stop(TransportCatalogue& catalogue, std::string_view stop_name) {
+    auto entry = 5;
+    stop_name = stop_name.substr(entry);
+    std::unordered_set<const Bus*> unique_buses;      
+    std::unordered_set<std::string_view> unique_buses_name;   
+    std::vector <std::string> bus_name_v;
+ 
+    Stop* stop = catalogue.get_stop(stop_name);
+ 
+    if (stop != NULL) {
+        unique_buses = catalogue.stop_get_uniq_buses(stop);
+ 
+        if(unique_buses.size() == 0){
+            std::cout << "Stop " << stop_name << ": no buses" << std::endl;
         }
-        else
-        {
-            size_t stop_on_route = transport_catalogue.get_stop_count(parsed.what);
-            size_t unique_stop_on_route = transport_catalogue.get_unique_stop_count(parsed.what);
-            double route_length = transport_catalogue.get_route_length(parsed.what);
-            output << stop_on_route << " stops on route, "sv;
-            output << unique_stop_on_route << " unique stops, "sv;
-            put_route_to_output(route_length, output);
-        }
-        return;
-    }
-    if (parsed.request == "Stop"sv)
-    {
-        auto stop = transport_catalogue.get_stop(parsed.what);
-        if (stop == nullptr)
-        {
-            output << "not found\n"sv;
-        }
-        else
-        {
-            std::set<std::string_view> bus_list;
-            for (const auto &bus : transport_catalogue.get_buses())
-            {
-                if (std::find(bus.stops.cbegin(), bus.stops.cend(), stop) != bus.stops.cend())
-                    bus_list.insert(bus.name);
+        else{
+            std::cout << "Stop " << stop_name << ": buses ";
+ 
+            for (const Bus* _bus : unique_buses) {
+                bus_name_v.push_back(_bus->name_);
             }
-            if (bus_list.empty())
-            {
-                output << "no buses\n";
-                return;
+ 
+            std::sort(bus_name_v.begin(), bus_name_v.end());         
+ 
+            for (std::string_view _bus_name : bus_name_v) {
+                std::cout << _bus_name;
+                std::cout << " ";
             }
-            output << "buses"sv;
-            for (const auto &bus : bus_list)
-            {
-                output << " "sv << bus;
-            }
-            output << "\n";
-        }
+            std::cout << std::endl;
+        }        
+    } 
+    else {      
+        std::cout << "Stop " << stop_name << ": not found" << std::endl;
     }
 }
-
-void put_route_to_output(double value, std::ostream &output)
-{
-    int precision = 1;
-    while (true)
-    {
-        ++precision;
-        if (value < std::pow(10, precision))
-            break;
+ 
+void query_(TransportCatalogue& catalogue, std::string_view str) {
+    if (str.substr(0, 3) == "Bus") {
+        query_bus(catalogue, str);
+    } else if (str.substr(0, 4) == "Stop") {
+        query_stop(catalogue, str);
+    } else{
+        std::cout << "Error query" << std::endl;
     }
-    output << std::fixed << std::setprecision(6 - precision) << value << " route length\n"sv;
+}
+ 
+void output_(TransportCatalogue& catalogue) {
+    std::string count;
+    std::getline(std::cin, count);
+ 
+    std::string str;
+    std::vector<std::string> query;
+    auto amount = stoi(count);
+ 
+    for (int i = 0; i < amount; ++i) {
+        std::getline(std::cin, str);
+        query.push_back(str);
+    }
+ 
+    for (auto& strr : query) {
+        query_(catalogue, strr);
+    }
 }
