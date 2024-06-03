79c05e09af20eafe62f2be6f2655cfbc416d9a38
neurastra <157818039+neurastra@users.noreply.github.com>
2024-06-03 10:56:19 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,23 +1,10 @@
 #pragma once
-
+ 
 #include <iosfwd>
 #include <string_view>
-#include <vector>
+ 
 #include "transport_catalogue.h"
-#include "input_reader.h"
-
-namespace Request
-{
-    struct Req
-    {
-        std::string_view request;
-        std::string_view what;
-    };
-
-    Req ParseRequest(const std::string_view request);
-
-    void ParseAndPrintStat(const Data::TransportCatalogue &tansport_catalogue, std::string_view request,
-                           std::ostream &output);
-}
-
-void put_route_to_output(double value, std::ostream &output);
+ 
+void ParseAndPrintStat(const TransportCatalogue &tansport_catalogue, std::string_view request,
+                       std::ostream &output);
+ 
