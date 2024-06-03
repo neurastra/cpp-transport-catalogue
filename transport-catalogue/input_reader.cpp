2b0b7da5ddcd3605c2c9c549c9305ff319e60c92
neurastra <157818039+neurastra@users.noreply.github.com>
2024-06-03 10:54:27 +0300

    Update input_reader.cpp

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -1,29 +1,26 @@
 #include "input_reader.h"
-
+ 
 #include <algorithm>
 #include <cassert>
 #include <iterator>
-
-using Geo::Coordinates;
-using namespace Input;
-
+ 
 Coordinates ParseCoordinates(std::string_view str)
 {
     static const double nan = std::nan("");
-
+ 
     auto not_space = str.find_first_not_of(' ');
     auto comma = str.find(',');
-
+ 
     if (comma == str.npos)
     {
         return {nan, nan};
     }
-
+ 
     auto not_space2 = str.find_first_not_of(' ', comma + 1);
-
+ 
     double lat = std::stod(std::string(str.substr(not_space, comma - not_space)));
     double lng = std::stod(std::string(str.substr(not_space2)));
-
+ 
     return {lat, lng};
 }
 
@@ -36,11 +33,11 @@ std::string_view Trim(std::string_view string)
     }
     return string.substr(start, string.find_last_not_of(' ') + 1 - start);
 }
-
+ 
 std::vector<std::string_view> Split(std::string_view string, char delim)
 {
     std::vector<std::string_view> result;
-
+ 
     size_t pos = 0;
     while ((pos = string.find_first_not_of(' ', pos)) < string.length())
     {
@@ -55,24 +52,24 @@ std::vector<std::string_view> Split(std::string_view string, char delim)
         }
         pos = delim_pos + 1;
     }
-
+ 
     return result;
 }
-
-std::vector<std::string_view> ParseRoute(std::string_view route)
+ 
+std::pair<RouteType, std::vector<std::string_view>> ParseRoute(std::string_view route)
 {
     if (route.find('>') != route.npos)
     {
-        return Split(route, '>');
+        return {RouteType::Ring, Split(route, '>')} ;
     }
-
+ 
     auto stops = Split(route, '-');
     std::vector<std::string_view> results(stops.begin(), stops.end());
     results.insert(results.end(), std::next(stops.rbegin()), stops.rend());
-
-    return results;
+ 
+    return {RouteType::ThereAndback, results};
 }
-
+ 
 CommandDescription ParseCommandDescription(std::string_view line)
 {
     auto colon_pos = line.find(':');
@@ -80,24 +77,24 @@ CommandDescription ParseCommandDescription(std::string_view line)
     {
         return {};
     }
-
+ 
     auto space_pos = line.find(' ');
     if (space_pos >= colon_pos)
     {
         return {};
     }
-
+ 
     auto not_space = line.find_first_not_of(' ', space_pos);
     if (not_space >= colon_pos)
     {
         return {};
     }
-
+ 
     return {std::string(line.substr(0, space_pos)),
             std::string(line.substr(not_space, colon_pos - not_space)),
             std::string(line.substr(colon_pos + 1))};
 }
-
+ 
 void InputReader::ParseLine(std::string_view line)
 {
     auto command_description = ParseCommandDescription(line);
@@ -106,13 +103,38 @@ void InputReader::ParseLine(std::string_view line)
         commands_.push_back(std::move(command_description));
     }
 }
-
-void InputReader::ApplyCommands([[maybe_unused]] Data::TransportCatalogue &catalogue) const
+ 
+void InputReader::ApplyCommands([[maybe_unused]] TransportCatalogue& catalogue) const
 {
-    for (const auto &command : commands_)
-        if (command.command == "Stop")
-            catalogue.add_stop(std::move(command.id), std::move(ParseCoordinates(command.description)));
-    for (const auto &command : commands_)
-        if (command.command == "Bus")
-            catalogue.add_bus(std::move(command.id), ParseRoute(command.description));
+    using namespace std::literals;
+ 
+    std::sort(commands_.begin(), commands_.end(),
+    [](const CommandDescription& lhs, const CommandDescription& rhs){
+        if(lhs.command == "Stop"s) return true;
+        if (lhs.command == "Bus"s && rhs.command == "Stop"s) return false;
+        if (lhs.command == "Bus"s && rhs.command == "Bus"s) return lhs.command < rhs.command;
+        return false;
+    });
+ 
+    for(const CommandDescription& command : commands_)
+    {
+        if(command.command == "Stop"s)
+        {
+            Stop stop{command.id, ParseCoordinates(command.description)};
+            catalogue.AddStop(std::move(stop));
+        }
+        if (command.command == "Bus"s)
+        {
+            auto [type, parsed_stops] = ParseRoute(command.description);
+            std::vector<std::string_view> route = parsed_stops;
+ 
+            std::vector<Stop*> stops;
+            for (const auto stop_name : route)
+            {
+                stops.push_back(catalogue.GetStopByName(stop_name));
+            }
+            Bus bus{command.id, stops, type};
+            catalogue.AddBus(std::move(bus));
+        }
+    }
 }
