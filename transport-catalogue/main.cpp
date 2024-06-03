29f42d257bce2fcbf2dfd6032687c073b3d3e148
neurastra <157818039+neurastra@users.noreply.github.com>
2024-06-03 10:55:21 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
 #include <string>
-
+ 
 #include "input_reader.h"
 #include "stat_reader.h"
-
+ 
 using namespace std;
-using namespace Input;
-
-int main() {
-    Data::TransportCatalogue catalogue;
-
+ 
+int main()
+{
+    TransportCatalogue catalogue;
+ 
     int base_request_count;
     cin >> base_request_count >> ws;
-
+ 
     {
         InputReader reader;
-        for (int i = 0; i < base_request_count; ++i) {
+        for (int i = 0; i < base_request_count; ++i)
+        {
             string line;
             getline(cin, line);
             reader.ParseLine(line);
         }
         reader.ApplyCommands(catalogue);
     }
-
+ 
     int stat_request_count;
     cin >> stat_request_count >> ws;
-    for (int i = 0; i < stat_request_count; ++i) {
+    for (int i = 0; i < stat_request_count; ++i)
+    {
         string line;
         getline(cin, line);
-        Request::ParseAndPrintStat(catalogue, line, cout);
+        ParseAndPrintStat(catalogue, line, cout);
     }
 }
