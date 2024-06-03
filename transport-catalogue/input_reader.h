59a5eb6169c7aaa55a7bf5d07755bd8f99a7f0b1
neurastra <157818039+neurastra@users.noreply.github.com>
2024-06-03 10:55:05 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -2,39 +2,42 @@
 #include <string>
 #include <string_view>
 #include <vector>
-#include <algorithm>
-#include <execution>
+ 
 #include "geo.h"
 #include "transport_catalogue.h"
-
-namespace Input
+ 
+struct CommandDescription
 {
-    struct CommandDescription
+    explicit operator bool() const
     {
-        explicit operator bool() const
-        {
-            return !command.empty();
-        }
-
-        bool operator!() const
-        {
-            return !operator bool();
-        }
-
-        std::string command;     
-        std::string id;          
-        std::string description; 
-    };
-
-    class InputReader
+        return !command.empty();
+    }
+ 
+    bool operator!() const
     {
-    public:
+        return !operator bool();
+    }
+ 
+    void operator=(const CommandDescription& other)
+    {
+        command = other.command;
+        id = other.id;
+        description = other.description;
+    }
+ 
+    std::string command;     
+    std::string id;          
+    std::string description; 
+};
+ 
+class InputReader
+{
+public:
 
-        void ParseLine(std::string_view line);
+    void ParseLine(std::string_view line);
 
-        void ApplyCommands(Data::TransportCatalogue &catalogue) const;
-
-    private:
-        std::vector<CommandDescription> commands_;
-    };
-}
+    void ApplyCommands(TransportCatalogue& catalogue) const;
+ 
+private:
+    std::vector<CommandDescription> commands_;
+};
